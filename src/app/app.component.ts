2b9d136593de7a179d73a77140c0c98fe9c03928
Artem Bolshak <Artem.Bolshak@mht.priv>
2022-01-04 10:07:17 +0200

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+export enum ControlName {
+  EMAIL = 'email',
+  PASSWORD = 'password',
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  [x: string]: any;
-  form: FormGroup;
-
-  constructor() {
-    this.form = new FormGroup({
-      email: new FormControl('', [
+  form: FormGroup = new FormGroup({
+      [ControlName.EMAIL]: new FormControl('', [
         Validators.email,
         Validators.required
       ]),
-      password: new FormControl('', [
+      [ControlName.PASSWORD]: new FormControl('', [
         Validators.required,
         Validators.minLength(6)
       ])
-    })
+    }
+  );
+
+  constructor() {
+    
   }
 
-  // ngOnInit(): void {
-  //     this.form = new FormGroup({})
-  // }
-
+  ngOnInit(): void {
+      // this.form = new FormGroup({})
+        // Validators.minLength(6)
+        // this.form.controls[ControlName.EMAIL].addValidators); 
+  }
   submit() {
-    if (this.form.valid) {
+    if (this.form?.valid) {
       console.log('Form: ', this.form);
       const formData = {
         ...this.form.value
